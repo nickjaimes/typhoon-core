54d2182050cc8772ff4fae4d834ccbe824d6ff74
nickjaimes <nickjaimes@gmail.com>
2025-11-07 00:49:55 +0900

    Initialize typhoon_core module with core components

---
+++ typhoon_core/_init_.py
@@ -0,0 +1,10 @@
+"""
+SAFEWAY GUARDIAN | Nicolas E. Santiago | Saitama, Japan | Nov. 6, 2025
+Powered by DEEPSEEK AI
+
+Core typhoon formation engine modules.
+"""
+
+from .engine import TyphoonCore, TyphoonConfig
+
+__all__ = ["TyphoonCore", "TyphoonConfig"]
